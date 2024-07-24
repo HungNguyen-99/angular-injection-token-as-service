e6e863db726ed5fe1907f82afc3e582cd3f44c46
hung99 <hungnvhe130726@gmail.com>
2024-07-24 13:45:33 +0700

    host

--- my-app/src/app/components/doc-angular-dev/components/test-resolution-modifier.component.ts
+++ my-app/src/app/components/doc-angular-dev/components/test-resolution-modifier.component.ts
@@ -1,78 +1,82 @@
-import { Component, Directive, inject } from '@angular/core';
+import { Component, Directive, Host, inject } from '@angular/core';
 import { TestResolutionModifierService } from '../services/test-resolution-modifier.service';
 
 @Component({
-  selector: 'app-child',
-  template: ` <h2>child</h2> `,
-  standalone: true,
-  imports: [],
-  providers: [],
+    selector: 'app-child',
+    template: ` <h2>child</h2> `,
+    standalone: true,
+    imports: [],
+    providers: [],
 })
 export class Child {
-  private testResolutionModifierService = inject(
-    TestResolutionModifierService,
-    { optional: true, skipSelf: true }
-  );
+    private testResolutionModifierService = inject(
+        TestResolutionModifierService,
+        { optional: true, skipSelf: true }
+    );
 
-  ngOnInit(): void {
-    this.testResolutionModifierService?.log('child');
-  }
+    ngOnInit(): void {
+        this.testResolutionModifierService?.log('child');
+    }
 }
 
 @Component({
-  selector: 'app-test-resolution-modifier',
-  template: `
+    selector: 'app-test-resolution-modifier',
+    template: `
     <h1>Parent</h1>
     <app-child />
   `,
-  standalone: true,
-  providers: [TestResolutionModifierService],
-  imports: [Child],
+    standalone: true,
+    providers: [TestResolutionModifierService],
+    imports: [Child],
 })
 export class TestResolutionModifierComponent {
-  private testResolutionModifierService = inject(TestResolutionModifierService);
+    private testResolutionModifierService = inject(TestResolutionModifierService);
 
-  ngOnInit(): void {
-    this.testResolutionModifierService.setCount(5);
-    this.testResolutionModifierService.log('parent');
-  }
+    ngOnInit(): void {
+        this.testResolutionModifierService.setCount(5);
+        this.testResolutionModifierService.log('parent');
+    }
 }
 
 // ========================Host=====================
+class LoggerService {
+    log(message: string) {
+        console.log(message);
+    }
+}
 @Directive({
-  selector: '[appChild]',
-  standalone: true,
+    selector: '[appChild]',
+    standalone: true,
 })
 export class ChildDirective {
-  constructor(private logger: LoggerService) {
-    this.logger.log('child directive');
-  }
+    constructor(@Host() private logger: LoggerService) {
+        this.logger.log('child directive');
+    }
 }
 @Directive({
-  selector: '[appParent]',
-  standalone: true,
+    selector: '[appParent]',
+    standalone: true,
+    providers: [LoggerService],
 })
 export class ParentDirective {
-  constructor(private logger: LoggerService) {
-    this.logger.log('parent directive');
-  }
+    constructor(private logger: LoggerService) {
+        this.logger.log('parent directive');
+    }
 }
-class LoggerService {
-  log(message: string) {
-    console.log(message);
-  }
-}
+
 @Component({
-  selector: 'app-test-resolution-modifier-host',
-  template: `
+    selector: 'app-test-resolution-modifier-host',
+    template: `
     <div appParent>
-      <div appChild>123</div>
+        <div>
+            <div appChild>123</div>
+        </div>
     </div>
   `,
-  standalone: true,
-  imports: [ParentDirective, ChildDirective],
-  providers: [LoggerService],
+    standalone: true,
+    imports: [ParentDirective, ChildDirective],
+    providers: [LoggerService],
 })
 export class TestResolutionModifierHostComponent {
-  ngOnInit(): void {}
+    ngOnInit(): void { }
 }
