94373e5b8ef48b970857836a98799cc01c2d42e0
hung99 <hungnvhe130726@gmail.com>
2024-07-29 10:50:35 +0700

    done CVA

--- my-app/src/app/components/control-value-accsessor/control-value-accessor-container.component.ts
+++ my-app/src/app/components/control-value-accsessor/control-value-accessor-container.component.ts
@@ -1,23 +1,39 @@
 import { JsonPipe, NgIf } from "@angular/common";
 import { Component } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { ProvinceSelect } from "./control-value-accessor.component";
 import { CvaRatingComponent } from "./cva-rating.component";
 
 @Component({
-    selector: 'control-value-accessor-container',
-    template: `
+  selector: 'control-value-accessor-container',
+  template: `
       <province-select [(ngModel)]="provinceModel" #provinceEl="ngModel" [type]="'central'"></province-select>
       <span>{{provinceEl.touched ? 'touched' : 'none touch'}}</span>
       <p style="color:red" *ngIf="provinceEl.invalid && provinceEl.errors && provinceEl && provinceEl.errors['type']">Type is not correct</p>
       <p>{{provinceEl.value | json}}</p>
       ============================================
-      <cva-rating [listStar]="listStar"] />
+      <cva-rating [listStar]="listStar" [formControl]="ratingFc"/>
     `,
-    standalone: true,
-    imports: [ProvinceSelect, CvaRatingComponent, FormsModule, NgIf, JsonPipe]
+  standalone: true,
+  imports: [ProvinceSelect, CvaRatingComponent, FormsModule, NgIf, JsonPipe, ReactiveFormsModule]
 })
 export class ControlValueAccessorContainer {
-    provinceModel: { id: number, name: string, type: 'central' | 'province' } = { "id": 4, "name": "Lào Cai", "type": "province" };
-    listStar = [1, 2, 3, 4, 5];
+  provinceModel: { id: number, name: string, type: 'central' | 'province' } = { "id": 4, "name": "Lào Cai", "type": "province" };
+  listStar = [1, 2, 3, 4, 5];
+  ratingFc = new FormControl(2);
+
+  ngOnInit(): void {
+    const rateStorage = localStorage.getItem('rate');
+    this.ratingFc.setValue(rateStorage ? +rateStorage : 0);
+    this.ratingFc.valueChanges.subscribe(value => {
+      console.log('value rating change:: ', value);
+      console.log(this.ratingFc.touched);
+      if (value !== null && value !== undefined) {
+        localStorage.setItem('rate', JSON.stringify(value));
+      }
+    })
+    if (this.ratingFc.value === 4) {
+      // this.ratingFc.disable();
+    }
+  }
 }
--- my-app/src/app/components/control-value-accsessor/cva-rating.component.ts
+++ my-app/src/app/components/control-value-accsessor/cva-rating.component.ts
@@ -1,4 +1,4 @@
-import { NgFor } from "@angular/common";
+import { NgClass, NgFor } from "@angular/common";
 import { Component, Input, OnInit } from "@angular/core";
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
 
@@ -7,9 +7,26 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
     template: `
         <p>CVA RATING</p>
         <div>
-            <!-- <span *ngFor="let item of listStar"></span> -->
+            <span *ngFor="let item of listStar; let index = index" [ngClass]="{'isStar': indexStart === index, 'nonStar': indexStart !== index}" (click)="rate(index)">★</span>
         </div>
     `,
+    styles: [
+        `
+            .isStar {
+                color: gold;
+                font-size: 24px;
+            }
+            .nonStar {
+                color: gray;
+                font-size: 24px;
+            }
+            div {
+                display: flex;
+                gap: 5px;
+                cursor: pointer;
+            }
+        `
+    ],
     standalone: true,
     providers: [
         {
@@ -18,26 +35,41 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
             multi: true
         }
     ],
-    imports: [NgFor]
+    imports: [NgFor, NgClass]
 })
 export class CvaRatingComponent implements OnInit, ControlValueAccessor {
 
+
     @Input() listStar: number[] = [];
 
-    ngOnInit(): void {
+    indexStart = 0;
+    isDisable = false;
+    onChange!: (index: number) => void;
+    onTouched!: () => void;
 
+    ngOnInit(): void {
     }
     writeValue(obj: any): void {
-
+        this.indexStart = obj;
     }
     registerOnChange(fn: any): void {
-
+        this.onChange = fn;
     }
     registerOnTouched(fn: any): void {
-
+        this.onTouched = fn;
     }
     setDisabledState?(isDisabled: boolean): void {
+        this.isDisable = isDisabled;
+    }
 
+    rate(index: number) {
+        if (this.isDisable) {
+            return;
+        }
+        // this.isDisable = index === 4;
+        this.indexStart = index;
+        this.onChange(index);
+        this.onTouched();
     }
 
 }
